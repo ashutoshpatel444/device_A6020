1e2cb0014239fdf7d00e53b8e1e3ec6ca98ad62f
harry8242 <mohamedharis97@gmail.com>
2017-08-11 14:38:14 +0000

    carbon bringup

--- A6020.mk
+++ A6020.mk
@@ -1,26 +1,30 @@
+#
 # Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/lenovo/A6020/full_A6020.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product-if-exists, vendor/slim/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += device/lenovo/A6020/overlay
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := A6020
-PRODUCT_NAME := slim_A6020
+# Include package config fragments
+include $(LOCAL_PATH)/product/*.mk
+
+$(call inherit-product-if-exists, vendor/lenovo/A6020/A6020-vendor.mk)
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 
-add_lunch_combo slim_A6020-userdebug
+add_lunch_combo carbon_A6020-userdebug
